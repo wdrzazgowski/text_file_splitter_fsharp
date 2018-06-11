24463e78864550a80d2a80ebc0068921d12996c9
Wojciech Drzazgowski <wojciech.drzazgowski@ihsmarkit.com>
2018-06-11 12:42:51 +0200

    added a function splitting a seq into smaller pieces of given size

--- Program.fs
+++ Program.fs
@@ -26,13 +26,53 @@ open System.Collections.Generic
         writeFile rest (1 + count)
 *)
 
+
+(*
+module Seq =
+
+    /// Returns a sequence that yields chunks of length n.
+    /// Each chunk is returned as a list.
+    let split length (xs: seq<'T>) =
+        let rec loop xs =
+            [
+                yield Seq.truncate length xs |> Seq.toList
+                match Seq.length xs <= length with
+                | false -> yield! loop (Seq.skip length xs)
+                | true -> ()
+            ]
+        loop xs
+
+// Demo
+[1 .. 20]
+|> Seq.split 3
+|> Seq.toArray
+
+// Output
+// [|[1; 2; 3]; [4; 5; 6]; [7; 8; 9]; [10; 11; 12]; [13; 14; 15]; [16; 17; 18];
+// [19; 20]|]
+*)
+
+
 let writeStringArrayToFile (lines: list<string>) (outputFileName: string) =
     use sw = new System.IO.StreamWriter(outputFileName)
     lines |> Seq.iter(sw.WriteLine)
     sw.Close()
 
+
+let rec readLinesIntoArray sr index = 
+    match sr.ReadLine with
+    | line -> 
+
 let readFile filePath =
-    File.ReadAllLines filePath
+    use sr = new System.IO.StreamReader(filePath)
+    let index = 0
+    
+    match sr.ReadLine() with
+    | line -> result[index / 1000].add line;  
+    | null -> sr.Close(); None 
+    
+    //File.ReadAllLines filePath
+
 [<EntryPoint>]
 let main argv =
     let inputFilePath = argv.[0]
@@ -45,6 +85,7 @@ let main argv =
     printfn "Writing file %s" outputFilePath
     
     let lineList = lines |> List.ofArray
+
     writeStringArrayToFile lineList outputFilePath |> ignore
 
     0 // return an integer exit code
